e74f7ab9efe5946c5dc0c27d1bfeaa3b9fa6113e
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-08-05 02:05:28 +0300

    deit and delete user feature added

---
+++ identity/Models/EditViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace identity.Models
+{
+    public class EditViewModel
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password missmatch")]
+        public string ConfirmPassword { get; set; }
+        public IList<string> SelectedRoles { get; set; }
+    }
+}
